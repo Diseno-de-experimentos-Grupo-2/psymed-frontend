78100a836bd4f7278fb2a39bff3258dce9128dbc
RominaMaita <u202213765@upc.edu.pe>
2025-10-04 16:26:58 -0500

    feat: remove start and end date fields from medication form

--- src/app/medication-management/modules/medication-form/medication-form.component.ts
+++ src/app/medication-management/modules/medication-form/medication-form.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Medication } from '../../models/medication.entity';
 import { MedicationService } from '../../services/medication.service';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import {MatDatepickerModule, MatDatepickerToggle} from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
-import {ActivatedRoute} from "@angular/router";
-import {NgIf} from "@angular/common";
-import {TranslateModule} from "@ngx-translate/core";
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { Medication } from '../../models/medication.entity';
 
 @Component({
   selector: 'app-medication-form',
@@ -17,66 +14,42 @@ import {TranslateModule} from "@ngx-translate/core";
   imports: [
     MatFormFieldModule,
     MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDatepickerToggle,
     ReactiveFormsModule,
     MatButtonModule,
-    NgIf,
     TranslateModule
   ],
   templateUrl: './medication-form.component.html',
   styleUrls: ['./medication-form.component.css']
 })
 export class MedicationFormComponent implements OnInit {
-  medicationForm!: FormGroup;  // we Define the form group
-  patientId!: number; // Define the patient ID as a number
-  constructor(private fb: FormBuilder, private medicationService: MedicationService, private route: ActivatedRoute) {
-  }
-
-  /**
-   * @description
-   * This method is called when the component is initialized.
-   */
+  medicationForm!: FormGroup;
+  patientId!: number;
+  constructor(private fb: FormBuilder, private medicationService: MedicationService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    console.log("on init");
-
-    this.patientId = +this.route.snapshot.paramMap.get('id')!; // Get the patient ID from the route using the ActivatedRoute service
-    console.log("Patient ID:", this.patientId); // Log the patient ID to the console for security
-
+    this.patientId = +this.route.snapshot.paramMap.get('id')!;
     this.medicationForm = this.fb.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
       interval: ['', Validators.required],
-      quantity: ['', [Validators.required, Validators.min(1)]],
-      startDate: ['', Validators.required],
-      endDate: ['', Validators.required]
+      quantity: ['', [Validators.required, Validators.min(1)]]
     });
   }
 
-  /**
-   * @description
-   * This method is called when the form is submitted.
-   */
   onSubmit(): void {
     if (this.medicationForm.valid) {
       const formValues = this.medicationForm.value;
-      const newMedication = new Medication({  // Create a new medication object using the form values
+      const newMedication = new Medication({
         name: formValues.name,
         description: formValues.description,
-        // startDate: formValues.startDate,
-        // endDate: formValues.endDate,
         interval: formValues.interval,
-        quantity: formValues.quantity.toString(),
-        patientId: this.patientId,
-        //status : 0
+        quantity: formValues.quantity,
+        patientId: this.patientId
+        // Los demás campos se asignan por defecto en el constructor
       });
-
-      this.medicationService.createMedication(newMedication, localStorage.getItem("authToken"))
+      this.medicationService.createMedication(newMedication, localStorage.getItem('authToken'));
     } else {
       console.error('Form is invalid');
     }
   }
-  protected readonly window = window; // Define the window object
 }
